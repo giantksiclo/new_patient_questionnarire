d4748bd71319446bddfd0562cb94820042491752
Giant K <giant@ksiclo.com>
2025-03-14 17:45:37 +0900

    상세한 차트 라이브러리 타입 선언 추가 및 typeRoots 설정

---
+++ src/types/chart.d.ts
@@ -0,0 +1,18 @@
+declare module 'chart.js' {
+  export const Chart: any;
+  export const CategoryScale: any;
+  export const LinearScale: any;
+  export const BarElement: any;
+  export const Title: any;
+  export const Tooltip: any;
+  export const Legend: any;
+  export const PointElement: any;
+  export const LineElement: any;
+  export const ArcElement: any;
+}
+
+declare module 'react-chartjs-2' {
+  export const Bar: React.FC<any>;
+  export const Pie: React.FC<any>;
+  export const Line: React.FC<any>;
+} 
